c97d11a2bf49c8de26b42444bbfdf5f8923ce690
josephambrozich <josephambrozich@gmail.com>
2022-04-13 20:43:31 -0400

    Updates seeder and views to use database
    resolves #40 and #41

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -8,13 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tool extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'name',
-        'type',
-        'price',
-        'id'
-    ];
-
+    public $timestamps = false;
 
     // output fillable properties into a descriptive string
     public function toString() {
