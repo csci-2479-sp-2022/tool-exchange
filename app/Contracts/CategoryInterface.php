1ff5bfa48cbdc1013e157f4c02ed5f31245addca
josephambrozich <45825585+josephambrozich@users.noreply.github.com>
2022-04-20 14:59:51 -0400

    Moves toward eloquent model for categoryService (#43)
    resolves #31 and moves to eloquent model

--- app/Contracts/CategoryInterface.php
+++ app/Contracts/CategoryInterface.php
@@ -3,12 +3,13 @@
 namespace App\Contracts;
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Collection;
 
 interface CategoryInterface
 {
 
     function getCategoryById(int $id): ?Category;
 
-    function getCategories(): array;
+    function getCategories(): Collection;
 
 }
