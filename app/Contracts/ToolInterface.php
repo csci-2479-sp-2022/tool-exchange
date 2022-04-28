b974dcda9204606837314e510d47db0e034b5ae4
Andrew Harris <aharri10@cscc.edu>
2022-04-27 20:18:03 -0400

    get controller, route, and tests working with toolService

--- app/Contracts/ToolInterface.php
+++ app/Contracts/ToolInterface.php
@@ -3,7 +3,7 @@
 namespace App\Contracts;
 
 use App\Models\Tool;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
 
 interface ToolInterface
 {
--- tests/Feature/SearchControllerTest.php
+++ tests/Feature/SearchControllerTest.php
@@ -4,57 +4,55 @@ namespace Tests\Feature;
 
 use App\Models\Tool;
 use App\Contracts\SearchResultInterface;
+use App\Contracts\ToolInterface;
 use App\Services\ToolService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
 use Tests\TestCase;
 use Mockery\MockInterface;
 
 
 class SearchControllerTest extends TestCase
 {
-    private $searchResults = [];
+    private Collection $searchResults;
     private MockInterface $ToolServiceSpy;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->searchResult = self::getSearchResult();
-        $this->toolServiceSpy = $this->spy(ToolService::class);
+        $this->searchResults = self::getSearchResult();
+        $this->toolServiceSpy = $this->spy(ToolInterface::class);
 }
 
-private static function getSearchResult(){
-    return [
-        Tool::make([
-            'id' => 1,
-            'name' => 'Hammer']),
-    ];
-}
-public function test_search_result() 
-{
-    //arrange 
-    $this -> toolServiceSpy -> shouldReceive('getTools')
-    ->once()
-    ->andReturn([
-        Tool::make([
-            'id' => 1,
-            'name' => 'Hammer',
-        ]),
-    
-    ]);
-    //act
-    $response = $this->get('/search-results');
-    //assert 
-    $response -> assertStatus(200);
-    $response -> assertViewHas('tools',
-    [   Tool::make([
-            'id' => 1,
-            'name' => 'Hammer',]),
-    ]);
-}
+    private static function getSearchResult(){
+        return collect([
+            Tool::make([
+                'id' => 1,
+                'name' => 'Hammer']),
+        ]);
+    }
+    public function test_search_result()
+    {
+        // $tool = Tool::make([
+        //     'id' => 1,
+        //     'name' => 'Hammer',
+        // ]);
+
+        //arrange
+        $this->toolServiceSpy->shouldReceive('getTools')
+        ->once()
+        // ->andReturn(collect());
+        ->andReturn($this->searchResults);
+        //act
+        $response = $this->get('/search-results');
+        //assert
+        $response -> assertStatus(200);
+        $response -> assertViewHas('tools', $this->searchResults);
+    }
 
 
 }
 
-    
+
 
